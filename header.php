003f6025e93f8c43d6a8be8f4b8f958bb7bd01b4
Lukas Andriuška <60364075+TheLuk1s@users.noreply.github.com>
2020-03-23 21:58:59 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,28 +1,52 @@
 <?php 
-	session_start();
-	include_once ('includes/functions.php');
+session_start();
+include_once 'includes/functions.php';
 ?>
-
-<!-- Version V0.01 | 2020-09-03 -->
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
+	
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<title>HEALTH+</title>
 		
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		
-		<!-- JQuery, BootstrapJS, PopperJS -->
-		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<!-- JQuery -->
+		<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
+		<!-- Bootstrap -->
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-		
-		<!-- Includes -->
-		<script src="/includes/JS/custom.js"></script>  
-		<link rel="stylesheet" href="/includes/stylesheet.css" type="text/css" media="screen" />
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+		<!-- Local CSS, JS -->
+		<link rel="stylesheet" href="/style.css" type="text/css" media="screen" />
+		<script src="/includes/custom.js"></script>  
 		
 	</head>
 	<body>
+		<div class="container-fluid px-0">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<a class="navbar-brand" href="/">HEALTH+</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+					<ul class="navbar-nav">
+						<!--<li class="nav-item active">
+							<a class="nav-link text-uppercase" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-uppercase" href="#">Features</a>
+						</li>-->
+						<li class="nav-item">
+							<a class="nav-link" href="/login.php"><i class="fas fa-sign-in-alt"></i></a>
+						</li>				
+						<li class="nav-item">
+							<a class="nav-link" href="/register.php"><i class="fas fa-user-plus"></i></a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
